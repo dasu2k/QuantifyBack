51f2ae479bfdb47702b8c23e21f567f681868a5f
Tulasidhar <tulasidharmulakaluri@gmail.com>
2024-01-02 00:04:21 +0530

    authentication
    authentication rocks

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 const express = require('express');
 
 const mysql = require('mysql');
-const mongoose = require('mongoose');
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const cors = require('cors');
 const app = express();
 
+
+const key="guts";
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const db = mysql.createConnection({
-    user:'sql12672428',
-    password:'5g7NmZGHhR',
-    database:'sql12672428',
-    host:'sql12.freesqldatabase.com'
+    user:'root',
+    password:'chicken',
+    database:'quantify',
+    host:'localhost',
+    port:'3306'
 })
 
 app.listen(6969 , ()=>{
     console.log("backend up on port 6969");
 })
 
+
+
 app.get("/" , (req,res)=>{
     return res.json("home page");
 })
 
+
+//fetch food entries
 app.get("/food",(req,res)=>{
     const q="SELECT * FROM food WHERE DATE(logged_at) = CURDATE()";
 
@@ -37,8 +44,23 @@ app.get("/food",(req,res)=>{
     })
 })
 
+
+//inserting new food data 
 app.post("/food",(req,res)=>{
-    const {name,calories,protein} = req.body;
+    const {name,calories,protein,email} = req.body;
+    
+    const userFinder = "SELECT * FROM users WHERE email = ?";
+    let user;
+    db.query(userFinder,[email] , (err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            user=Object.values(JSON.parse(JSON.stringify(data)));
+            console.log(user);
+        }
+    })
+
     const q="INSERT INTO food (name,calories,protein) VALUES (?,?,?)";
     
     db.query(q,[name,calories,protein],(err,data)=>{
@@ -52,3 +74,66 @@ app.post("/food",(req,res)=>{
 })
 
 
+
+//regitration 
+app.post("/user" , async (req,res) =>{
+    let {username , password , email} = req.body;
+    console.log("password being hashed: " + password);
+    let password1 = await bcrypt.hash(password,10);
+    const q = "INSERT INTO users (username,password,email) VALUES (?,?,?)";
+
+    db.query(q,[username,password1,email] , (err,data) =>{
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.json("inserted new user");
+        }
+    })
+})
+
+//login authentication
+app.post("/login",(req,res)=>{
+    const token = jwt.sign(req.body.email , key);
+    let dataTobeSent = {
+        token:'',
+        message:''
+    }
+    console.log("entered password:"+req.body.password);
+    console.log("entered email:"+req.body.email);
+
+    const query = "SELECT * FROM users WHERE email = (?)";
+    
+    
+    db.query(query,[req.body.email], async (err,data)=>{
+        if(err){
+            res.json(err);
+        }
+        else{
+            if(data.length > 0){
+                console.log("password in db " + data[0].password);
+                console.log("comparing db stored hashed pass and " + req.body.password);
+                const isPassValid = await bcrypt.compare(req.body.password , data[0].password);
+
+                console.log("comparision result:" , isPassValid);
+
+                if(isPassValid)
+                {
+                    dataTobeSent.token=token;
+                    dataTobeSent.message = "welcome "+ data[0].username;
+                    res.json(dataTobeSent);
+                }
+                else{
+                    dataTobeSent.token=null;
+                    dataTobeSent.message = "wrong password"
+                    res.json(dataTobeSent);
+                }
+            }
+            else{
+                dataTobeSent.token=null;
+                dataTobeSent.message = "user does'nt exist";
+                res.json(dataTobeSent);
+            }
+        }
+    })
+})
